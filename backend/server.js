83b66c406d6d588690f7f3be775e5f7308e9125c
Admin <admin@erins-imac.dsgraphics.com>
2024-05-28 15:55:23 -0400

    user auth work

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 
-// Cookie parser middleware
-app.use(cookieParser());
+
 
 app.use(notFound);
 app.use(errorHandler);
