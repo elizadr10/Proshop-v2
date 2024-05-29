a6876a66de463ca05ccd2732d3be5c06d173a42f
Admin <admin@erins-imac.dsgraphics.com>
2024-05-29 09:42:54 -0400

    frontend submod fix

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,82 @@
+// import React from 'react'
+import { Navbar, Nav, Container, Badge, NavDropdown } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from '../assets/logo.png'
+import { useSelector } from 'react-redux'
+
+const Header = () => {
+    const { cartItems } = useSelector((state) => state.cart);
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const logoutHandler = () => {
+      console.log('logout');
+    }
+  return (
+    <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+            <LinkContainer to="/">
+                <Navbar.Brand>
+                    <img src={logo} alt="ProShop" />
+                    Proshop
+                </Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                  {
+                    cartItems.length > 0 && (
+                        <Badge pill bg='success' style={{margineLeft: '5px'}}>
+                            { cartItems.reduce((a, c) => a + c.qty, 0)}
+
+                        </Badge>
+                    )
+                  }
+                </Nav.Link>
+              </LinkContainer>
+              {/* { userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                <Nav.Link href='/login'>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>  
+              )} */}
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <FaUser /> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+              
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Header;
